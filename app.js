bf51e755e6480af0c25ee76a1fa749a7c5178545
korra <korrasnipes@gmail.com>
2021-08-20 17:39:44 +0300

    added space

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const app = express();
 
 const router = express.Router();
 
-router.get("/home", (req, res) => {
+router.get("/home", (req,res) => {
     res.send("helloworls, this is the home router");
 });
 
-router.get("/login", (req, res) => {
+router.get("/login", (req,res) => {
     res.send("helloworld, this is the login router");
 });
 
-router.get("/logout", (req, res) => {
+router.get("/logout", (req,res) => {
     res.send("helloworld, this is the logout router");
 });
 
