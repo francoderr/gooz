2e9ab21b8dec95933a6dd61d7be5b974ca289155
korra <korrasnipes@gmail.com>
2021-08-20 17:26:31 +0300

    added routes

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
+const router = express.Router();
+
+router.get("/home", (req, res) => {
+    res.send("helloworls, this is the home router");
 });
+
+router.get("/login", (req, res) => {
+    res.send("helloworld, this is the login router");
+});
+
+router.get("/logout", (req, res) => {
+    res.send("helloworld, this is the logout router");
+});
+
+app.use("/", router);
 
 app.listen(8080);
 console.log("server is running");
